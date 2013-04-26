37427062d97f0a51170defd58d881cf6d3afdd2a
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-04-26 09:13:15 +0300

    HTML build implemented

--- bemuse.js
+++ bemuse.js
@@ -3,15 +3,25 @@
     var blocks = {},
         MOD_NAME_SEPARATOR = '--',
         ELEMENT_NAME_SEPARATOR = '__',
-        EMPTY_TAGS = _.reduce([
+        EMPTY_TAGS = _([
             'area', 'base', 'br',
             'col', 'hr', 'img',
             'input', 'link', 'meta', 'param'
-        ], function (obj, tag) {
+        ]).reduce(function (obj, tag) {
             obj[tag] = true;
             return obj;
         }, {});
 
+    /**
+     * Returns true for objects
+     * Underscore's isObject returns true for functions and arrays.
+     * @param {Mixed} arg
+     *
+     * @returns {Boolean}
+     */
+    function isObject(arg) {
+        return Object.prototype.toString.call(arg) === '[object Object]';
+    }
     /**
      * Returns HTML class for modification
      *
@@ -31,20 +41,26 @@
         return [blockName, element].join(ELEMENT_NAME_SEPARATOR);
     }
 
+    /**
+     * New block constructor
+     * @param {String} name Block's name
+     *
+     * @return {Object}
+     */
     function Block(name) {
         this.name = name;
-
-        if (!blocks[this.name]) {
-            blocks[this.name] = {};
-        }
     }
     Block.prototype = {
+        /**
+         * Declare new block,
+         * if it already exists then extend it
+         *
+         * @param {Object} description
+         * @param {Object} [description.DOM]
+         * @param {Object} [description.HTML]
+         */
         declare: function (description) {
-            if (blocks[this.name]) {
-                _.extend(blocks[this.name], description);
-            } else {
-                blocks[this.name] = new Block(description);
-            }
+            _(this).extend(description);
         },
         inherits: [],
         DOM: {
@@ -60,7 +76,13 @@
 
     root.Bemuse = {
         Block: function (blockName) {
-            return new Block({name: blockName});
+            if (!blocks[blockName]) {
+                blocks[blockName] = new Block(blockName);
+            }
+            return blocks[blockName];
+        },
+        DOM: {
+            initOn: 'load'
         },
         HTML: {
             /**
@@ -68,89 +90,116 @@
              * using Block's descriptions
              *
              * @param {Object} bemjson
-             * @param {Object} [parent = bemjson] required for recursion
-             * @param {Object} [root = bemjson]  closest parent node with "block" property
+             * @param {Boolean} [manualInit = false]  Bemuse will add extra
+             * script tag to initialize new blocks on insert. But you
+             * can turn off it with manualInit = true
              *
              * @returns {String}
              */
-            build: function (bemjson, parent, root) {
-                return this.jsonToHtml(this.bemjsonToHtmljson(
-                    bemjson,
-                    parent,
-                    root
-                ));
+            build: function (bemjson, manualInit) {
+                var initOnInsert = {},
+                    htmljson = this.bemjsonToHtmljson(bemjson, initOnInsert);
+
+                if (!manualInit) {
+                    this._addInitScript(htmljson);
+                }
+                return this.jsonToHtml(htmljson);
+            },
+            /**
+             * @param {Array|Object} json
+             * @param {Array} [blocks]
+             */
+            _addInitScript: function (json) {
+                if (_(json).isArray()) {
+                    _(json).forEach(this._addInitScript);
+                } else if (_(json).isObject()) {
+                    if (!json.content) {
+                        json.content = [];
+                    }
+                    // json.content = _(json.content).concat('zzzzz');
+                }
             },
             /**
              * Converts BEM-oriented json into HTML-oriented json
              *
              * @param {Mixed} bemjson
-             * @param {Object} [parent = bemjson] required for recursion
-             * @param {Object} [root = bemjson]  closest parent node with "block" property
+             * @param {Object} [root = bemjson] closest parent node
+             * with "block" property
+             * @param {Object} initOnInsert Empty object,
+             * that will be filled with block names that
+             * should be inited on insert
              *
              * @returns {Mixed}
              */
-            bemjsonToHtmljson: function (bemjson, parent, root) {
-                parent = parent || bemjson;
-                root = root || bemjson;
+            bemjsonToHtmljson: function (bemjson, root, initOnInsert) {
+                var args = [].slice.call(arguments),
+                    block, mods, declarationNames, element;
 
-                var block = root.block || this.name,
-                    mods = (block && (root.mods || this.mod)) || [],
-                    declNames,
-                    element;
+                initOnInsert = args.pop();
+                root = args[1] || bemjson;
 
-                if (bemjson.element || bemjson.block) {
-                    // build array of all declaration names
-                    // to check against current bemjson
-                    if (mods.length) {
-                        declNames = mods.map(canonicalModName.bind(null, block));
-                    } else {
-                        declNames  = [];
-                    }
-                    declNames.push(block);
+                if (_(bemjson).isArray()) {
+                    return _(bemjson).map(function (child, i) {
+                        return bemjson[i] = this.bemjsonToHtmljson(
+                            bemjson[i], root, initOnInsert
+                        );
+                    }, this);
+                }
 
-                    if (bemjson.element) {
-                        // process element
-                        element = bemjson.element;
-                        this._applyDeclarations(bemjson, declNames, element);
-                        this._addClass(bemjson, canonicalElementName(element));
-                    } else {
-                        // process block
-                        this._applyDeclarations(bemjson, declNames);
-                        this._addClass(bemjson, block);
-                        this._addClass(bemjson, mods.map(
-                            canonicalModName.bind(null, block)
-                        ).join(' '));
-                    }
+                if (bemjson.block) {
+                    block = bemjson.block;
+                    mods = bemjson.mods || [];
+
+                    declarationNames = mods.map(
+                        canonicalModName.bind(null, block)
+                    ).concat(block);
+
+                    declarationNames.forEach(function (name) {
+                        if (blocks[name] && blocks[name].DOM.initOn === 'load') {
+                            initOnInsert[name] = true;
+                        }
+                    });
+
+                    this._applyDeclarations(bemjson, declarationNames);
+                    this._addClass(bemjson, declarationNames);
+                } else if (bemjson.element) {
+                    block = root.block || this.name;
+                    mods = root.mods || this.mod || [];
+                    element = bemjson.element;
+
+                    declarationNames = mods.map(
+                        canonicalModName.bind(null, block)
+                    ).concat(block);
+
+                    this._applyDeclarations(bemjson, declarationNames, element);
+                    this._addClass(bemjson, canonicalElementName(block, element));
                 }
                 if (bemjson.wrapper) {
-                    if (_.isObject(bemjson.wrapper)) {
+                    if (isObject(bemjson.wrapper)) {
                         bemjson.wrapper.content = bemjson;
                     }
-                    return (parent.content = this.bemjsonToHtmljson(
-                        bemjson.wrapper,
-                        parent,
-                        root
-                    ));
-                } else if (bemjson.content) {
-                    return this.bemjsonToHtmljson(
-                        bemjson.content,
-                        bemjson,
-                        // closest parent with block property
-                        bemjson.block ? bemjson:root
+                    bemjson = this.bemjsonToHtmljson(
+                        bemjson.wrapper, root, initOnInsert
                     );
-                } else {
-                    return bemjson;
+                } else if (bemjson.content) {
+                    bemjson.content = this.bemjsonToHtmljson(
+                        bemjson.content,
+                        // closest parent with block property
+                        bemjson.block ? bemjson:root,
+                        initOnInsert
+                    );
                 }
+                return bemjson;
             },
             /**
              * Apply Block's HTML declarations to bemjson.
              *
              * @param {Object} bemjson
-             * @param {Array<String>} declNames canonical names of block with all mods
+             * @param {Array<String>} declarationNames canonical names of block with all mods
              * @param {String} [element] If ommited, process onBlock
              */
-            _applyDeclarations: function (bemjson, declNames, element) {
-                declNames.some(function (name) {
+            _applyDeclarations: function (bemjson, declarationNames, element) {
+                declarationNames.some(function (name) {
                     var declaration = blocks[name],
                         handler;
 
@@ -174,13 +223,15 @@
              * @param {String} className
              */
             _addClass: function (htmljson, className) {
-                htmljson.attrs = htmljson.attrs || {};
-                if (htmljson.attrs['class']) {
-                    htmljson.attrs['class'] += ' ';
-                } else {
-                    htmljson.attrs['class'] = '';
+                if (className) {
+                    htmljson.attrs = htmljson.attrs || {};
+                    if (htmljson.attrs['class']) {
+                        htmljson.attrs['class'] += ' ';
+                    } else {
+                        htmljson.attrs['class'] = '';
+                    }
+                    htmljson.attrs['class'] += className;
                 }
-                htmljson.attrs['class'] += className;
             },
             /**
              * Converts attrs object into html string
@@ -190,7 +241,7 @@
              * @returns {String}
              */
             attrsToHtml: function (attrs) {
-                return _.reduce(attrs, function (memo, value, name) {
+                return _(attrs).reduce(function (memo, value, name) {
                     value = value.replace(/"/g, '&quot;');
                     return memo + [' ', name, '="', value, '"'].join('');
                 }, '');
@@ -204,10 +255,10 @@
             jsonToHtml: function (bemjson) {
                 var tag, attrs, content, result;
 
-                if (_.isArray(bemjson)) {
+                if (_(bemjson).isArray()) {
                     return bemjson.map(this.jsonToHtml.bind(this)).join('');
                 // all logic goes here
-                } else if (_.isObject(bemjson)) {
+                } else if (_(bemjson).isObject()) {
                     tag = bemjson.tag || 'div';
                     attrs = bemjson.attrs || {};
 
--- tests.js
+++ tests.js
@@ -51,8 +51,76 @@ test('HTML build', function () {
             }
         }
     });
-    console.log(Bemuse.HTML.build({
-        block: 'test'
-    }));
-    equal('', '', '');
+    equal(
+        Bemuse.HTML.build({block: 'test'}),
+        '<span class="test"><div class="test-2"></div></span>',
+        'Block declaration works'
+    );
+
+    Bemuse.Block('test-2').declare({
+        HTML: {
+            onElement: {
+                title: function (tree) {
+                    tree.tag = 'span';
+                    tree.content = 'Hello';
+                }
+            }
+        }
+    });
+    equal(
+        Bemuse.HTML.build({block: 'test-2', content: {element: 'title'}}),
+        '<div class="test-2"><span class="test-2__title">Hello</span></div>',
+        'Element declaration works'
+    );
+
+    Bemuse.Block('test-3').declare({
+        HTML: {
+            onBlock: function (tree) {
+                tree.wrapper = {
+                    block: 'test'
+                };
+            }
+        }
+    });
+    equal(
+        Bemuse.HTML.build({block: 'test-3'}),
+        "<span class=\"test\"><div class=\"test-2\"></div></span>",
+        'Wrapper works'
+    );
+
+    Bemuse.Block('test-4').declare({
+        HTML: {
+            onBlock: function (tree) {
+                tree.wrapper = [];
+            }
+        }
+    });
+    equal(
+        Bemuse.HTML.build({block: 'test-4'}),
+        '',
+        'Wrap with array to remove'
+    );
+
+    equal(
+        Bemuse.HTML.build([
+            {block: 'test'},
+            {block: 'test-2'},
+            {block: 'test-3'}
+        ]),
+        '<span class="test"><div class="test-2"></div></span><div class="test-2"></div><span class="test"><div class="test-2"></div></span>',
+        'Arrays supported'
+    );
 });
+module('Bemuse DOM');
+test('DOM init', function () {
+    equal(
+        Bemuse.HTML.build([
+            {block: 'test'},
+            {block: 'test-2'},
+            {block: 'test-3'}
+        ]),
+        '<span class="test"><div class="test-2"></div></span><div class="test-2"></div><span class="test"><div class="test-2"></div></span>',
+        'Wrap with array to remove'
+    );
+});
+
